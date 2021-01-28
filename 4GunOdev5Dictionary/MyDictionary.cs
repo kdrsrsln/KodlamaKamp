55ee45413e6aaa108c7d00a3971431aac3c45ea6
Kadir Sarıaslan <software@sariaslan.net>
2021-01-28 16:55:34 +0300

    Add project files.

---
+++ 4GunOdev5Dictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4GunOdev5Dictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        private TKey[] keys;
+        private TValue[] values;
+
+        public MyDictionary()
+        {
+            // Creates empty arrays for Keys and Values
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            // Copies arrays to temp ones
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            // Creates new bigger arrays
+            keys = new TKey[keys.Length +1];
+            values = new TValue[values.Length +1];
+
+            // Copy Back from temp to new array
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            // Adds new items to arrays
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            Console.WriteLine("Key : " + key + " and Value : " + value + " added!" );
+
+        }
+    }
+}
---
+++ 4GunOdev5Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4GunOdev5Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyDictionary<int,string> StudentList = new MyDictionary<int, string>();
+            StudentList.Add(1,"Kadir");
+            StudentList.Add(2,"Burak");
+
+
+        }
+    }
+}
